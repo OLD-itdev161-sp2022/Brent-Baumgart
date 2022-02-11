930799a8aee9d70e36e07b23084ce5cff7cdd6f5
brent90 <brentjbaumgart@gmail.com>
2022-02-11 14:59:14 -0600

    added user registration api endpoint

--- server.js
+++ server.js
@@ -7,9 +7,27 @@ const app = express();
 //connect to mongodb
 connectDatabase();
 
+//middleware
+app.use(express.json({ extended: false}));
+
+/**
+ * @route Get /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) => {
-    res.send('Hello from express server');
-})
+    res.send('http get request sent to root api endpoint');
+});
+
+
+/**
+ * @route POST api/users
+ * @desc Register user
+ */
+ app.post('/api/users', (req, res) => {
+     console.log(reg.body);
+    res.send(req.body);
+});
+
 
 app.listen(3000, () => console.log(`Express running on port 3000`));
 
