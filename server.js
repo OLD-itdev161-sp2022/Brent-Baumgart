8de598e186929bbb9ed415c5619e0fef960dcf94
brent90 <brentjbaumgart@gmail.com>
2022-02-11 15:31:14 -0600

    update package.json and add validation

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import connectDatabase from './config/db';
-
+import {check, validationResult} from 'express-validator';
 
 const app = express();
+
+
+// **NOTE TO START **
+//on linux I need this, on windows I don't
+//nodemon --experimental-modules --es-module-specifier-resolution=node server.js 
+
+
 
 //connect to mongodb
 connectDatabase();
@@ -23,11 +30,22 @@ app.get('/', (req, res) => {
  * @route POST api/users
  * @desc Register user
  */
- app.post('/api/users', (req, res) => {
-     console.log(reg.body);
-    res.send(req.body);
-});
 
+ app.post('/api/users',
+ [
+     check('name', 'Please enter your name').not().isEmpty(),
+     check('email', 'Please enter a valid email').isEmail(),
+     check('password', 'Please enter a password with 6 or more characters').isLength({min:6})
+ ],
+ (req,res) => {
+     const errors = validationResult(req);
+     if (!errors.isEmpty()){
+         return res.status(422).json({errors:errors.array()});
+     } else{
+     return res.send(req.body);
+     }
+ });
+ 
 
 app.listen(3000, () => console.log(`Express running on port 3000`));
 
