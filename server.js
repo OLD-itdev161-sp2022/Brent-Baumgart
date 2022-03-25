7cd92aaa08e39b10a954a422687a589741e5369d
brent90 <brentjbaumgart@gmail.com>
2022-03-25 14:10:15 -0500

    added auth middleware and endpoints

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import User from './models/User';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
-
+import auth from './middleware/auth';
 
 const app = express();
 
@@ -21,6 +21,83 @@ app.use(
         origin:'http://localhost:3000'
     })
 );
+
+/**
+ * @route GET api/auth
+ * @desc Authorize user
+ */
+ app.get('/api/auth', auth, async (req, res) => {
+    try {
+      const user = await User.findById(req.user.id);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).send('Unknown server error');
+    }
+  });
+
+
+/**
+ * @route POST api/login
+ * @desc Login user
+ */
+ app.post(
+    '/api/login',
+    [
+      check('email', 'Please enter a valid email').isEmail(),
+      check('password', 'A password is required').exists()
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+      } else {
+        const { email, password } = req.body;
+        try {
+          // Check if user exists
+          let user = await User.findOne({ email: email });
+          if (!user) {
+            return res
+              .status(400)
+              .json({ errors: [{ msg: 'Invalid email or password' }] });
+          }
+  
+          // Check password
+          const match = await bcrypt.compare(password, user.password);
+          if (!match) {
+            return res
+              .status(400)
+              .json({ errors: [{ msg: 'Invalid email or password' }] });
+          }
+  
+          // Generate and return a JWT token
+          returnToken(user, res);
+        } catch (error) {
+          res.status(500).send('Server error');
+        }
+      }
+    }
+  );
+  
+
+  const returnToken = (user, res) => {
+    const payload = {
+      user: {
+        id: user.id
+      }
+    };
+  
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: '10hr' },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token: token });
+      }
+    );
+  };
+
+
 /**
  * @route Get /
  * @desc Test endpoint
@@ -73,23 +150,9 @@ app.get('/', (req, res) => {
 
                 // Save to the db and return
                 await user.save();
-
-              // Generate and return a JWI token
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get("jwtSecret"),
-                    { expiresIn: '10hr'},
-                    (err, token) => {
-                        if(err) throw err;
-                        res.json({ token: token });
-                    }
-                ); 
+            
+                // Generate and return a JWT token
+                returnToken(user, res);
 
                 console.log("new user added");
             } catch (error) {
